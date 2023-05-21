70dc0e1f5581701fe00accdc6e4bd1f117be37ea
Brian Lemba <72974932+Yakumwamba@users.noreply.github.com>
2023-05-21 18:00:21 +0200

    tailwind config fix

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -10,38 +10,38 @@ export default {
         poppins: ['Poppins', 'sans-serif'],
         notoserif: ['NotoSerif', 'sans-serif'],
         KumbhSans: ['KumbhSans', 'sans'],
-        
+
       },
 
-    screens: {
-      'sm': '370px',
-      // => @media (min-width: 640px) { ... }
+      screens: {
+        'sm': '370px',
+        // => @media (min-width: 640px) { ... }
 
-      'md': '768px',
-      // => @media (min-width: 768px) { ... }
+        'md': '768px',
+        // => @media (min-width: 768px) { ... }
 
-      'lg': '1024px',
-      // => @media (min-width: 1024px) { ... }
+        'lg': '1024px',
+        // => @media (min-width: 1024px) { ... }
 
-      'xl': '1280px',
-      // => @media (min-width: 1280px) { ... }
+        'xl': '1280px',
+        // => @media (min-width: 1280px) { ... }
 
-      '2xl': '1536px',
-      // => @media (min-width: 1536px) { ... }
-    },
+        '2xl': '1536px',
+        // => @media (min-width: 1536px) { ... }
+      },
       colors: {
         primary: {
           // Customize it on globals.css :root
           50: 'rgb(var(--tw-color-primary-50) / <alpha-value>)',
           100: 'rgb(var(--tw-color-primary-100) / <alpha-value>)',
-        
+
         },
         green1: "#003D2D",
         green2: "#365B51",
         dark: '#222222',
       },
-   
+
     },
   },
-  plugins: [require('flowbite/plugin')],
+  plugins: [],
 } satisfies Config;
