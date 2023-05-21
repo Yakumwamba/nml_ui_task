303cc09a4d4751db5429e50c8d700a99846ef708
Brian Lemba <72974932+Yakumwamba@users.noreply.github.com>
2023-05-21 17:51:47 +0200

    Fixing types

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,7 +2,7 @@ import { AppProps } from 'next/app';
 
 import '@/styles/globals.css';
 
-function MyApp({ Component, pageProps }) {
+function MyApp({ Component, pageProps }): AppProps {
   return <Component {...pageProps} />;
 }
 
